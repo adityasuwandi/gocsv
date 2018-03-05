c3e4868c487c9e9080184d63154bcf7a74b70923
haruelrovix <havitchoirul.rovik@mitrais.com>
2018-03-06 00:11:41 +0700

    Simple CSV writer

--- main.go
+++ main.go
@@ -1,13 +1,56 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Contact struct {
+	Email string
+	Open  int64
+	Link  int64
+}
+
+type Contacts []Contact
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.POST("/ping", func(c *gin.Context) {
+		contactsData := c.PostForm("data")
+
+		var contacts Contacts
+		json.Unmarshal([]byte(contactsData), &contacts)
+
+		b := &bytes.Buffer{}
+		w := csv.NewWriter(b)
+
+		if err := w.Write([]string{"email", "opens"}); err != nil {
+			log.Fatalln("error writing record to csv: ", err)
+		}
+
+		for _, contact := range contacts {
+			var record []string
+			record = append(record, contact.Email)
+			record = append(record, strconv.FormatInt(contact.Open, 10))
+			if err := w.Write(record); err != nil {
+				log.Fatalln("error writing record to csv: ", err)
+			}
+		}
+		w.Flush()
+
+		if err := w.Error(); err != nil {
+			log.Fatal(err)
+		}
+
+		c.Header("Content-Description", "File Transfer")
+		c.Header("Content-Disposition", "attachment; filename=contacts.csv")
+		c.Data(http.StatusOK, "text/csv", b.Bytes())
 	})
 	r.Run() // Listen and serve on 0.0.0.0:8080
 }
