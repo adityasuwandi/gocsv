6510fd0d673b6d4fbb05679ffccd63c9f2341848
haruelrovix <havitchoirul.rovik@mitrais.com>
2018-03-06 13:09:11 +0700

    CSV Writer for Laporan Nilai Barang

--- main.go
+++ main.go
@@ -6,30 +6,41 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Item type with SKU, Name and Total
-type Item struct {
-	SKU   string `json:"sku"`
-	Name  string `json:"name"`
-	Total int    `json:"total"`
+// Laporan Nilai Barang
+type itemReport struct {
+	SKU    string  `json:"sku"`
+	Name   string  `json:"name"`
+	Amount int     `json:"amount"`
+	Price  float64 `json:"price"`
+	Value  float64 `json:"value"`
 }
 
-type Items struct {
-	Items []Item `json:"items"`
+type summary struct {
+	PrintDate   string  `json:"printdate"`
+	TotalSKU    int     `json:"totalsku"`
+	TotalAmount int     `json:"totalamount"`
+	TotalValue  float64 `json:"totalvalue"`
 }
 
-type UntypedJson map[string][]interface{}
+type report struct {
+	Items   []itemReport `json:"items"`
+	Summary summary      `json:"summary"`
+}
+
+var w *csv.Writer
 
 func main() {
 	r := gin.Default()
-	r.GET("/items", func(c *gin.Context) {
+	r.GET("/nilaibarang", func(c *gin.Context) {
 		// Make a get request
-		rs, err := http.Get("http://0.0.0.0:3000/api/items")
+		rs, err := http.Get("http://0.0.0.0:3000/api/nilaibarang")
 
 		// Process response
 		if err != nil {
@@ -44,34 +55,59 @@ func main() {
 
 		bodyString := string(bodyBytes)
 
-		var result Items
+		var result report
 		json.Unmarshal([]byte(bodyString), &result)
 
 		b := &bytes.Buffer{}
-		w := csv.NewWriter(b)
+		w = csv.NewWriter(b)
 
-		if err := w.Write([]string{"SKU", "Nama Item", "Jumlah Sekarang"}); err != nil {
-			log.Fatalln("error writing record to csv: ", err)
-		}
+		// Summary
+		writeCSV(5, []string{"LAPORAN NILAI BARANG"})
+		writeCSV(5, []string{})
+		writeCSV(5, []string{"Tanggal Cetak", result.Summary.PrintDate})
+		writeCSV(5, []string{"Jumlah SKU", strconv.Itoa(result.Summary.TotalSKU)})
+		writeCSV(5, []string{"Jumlah Total Barang", strconv.Itoa(result.Summary.TotalAmount)})
+		writeCSV(5, []string{"Total Nilai", "Rp" + strconv.Itoa(int(math.Round(result.Summary.TotalValue)))})
+		writeCSV(5, []string{})
 
+		// Header
+		writeCSV(5, []string{
+			"SKU",
+			"Nama Item",
+			"Jumlah",
+			"Rata-Rata Harga Beli",
+			"Total",
+		})
+
+		// Content
 		for _, value := range result.Items {
 			var record []string
 			record = append(record, value.SKU)
 			record = append(record, value.Name)
-			record = append(record, strconv.Itoa(value.Total))
-			if err := w.Write(record); err != nil {
-				log.Fatalln("error writing record to csv: ", err)
-			}
+			record = append(record, strconv.Itoa(value.Amount))
+			record = append(record, strconv.Itoa(int(math.Round(value.Price))))
+			record = append(record, strconv.Itoa(int(math.Round(value.Value))))
+			writeCSV(5, record)
 		}
-		w.Flush()
 
+		w.Flush()
 		if err := w.Error(); err != nil {
 			log.Fatal(err)
 		}
 
-		c.Header("Content-Description", "Catatan Jumlah Barang")
-		c.Header("Content-Disposition", "attachment; filename=toko.csv")
-		c.Data(http.StatusOK, "text/csv; charset=utf-8", b.Bytes())
+		c.Header("Content-Description", "Laporan Nilai Barang")
+		c.Header("Content-Disposition", "attachment; filename=nilai_barang.csv")
+		c.Data(http.StatusOK, "text/csv", b.Bytes())
 	})
 	r.Run() // Listen and serve on 0.0.0.0:8080
+}
+
+func writeCSV(column int, values []string) {
+	record := make([]string, column)
+	for i, value := range values {
+		record[i] = value
+	}
+	if err := w.Write(record); err != nil {
+		log.Fatalln("error writing record to csv: ", err)
+	}
 }
